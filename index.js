4b8284a23c3108a22dbedf49efe3f4d7804a99e7
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-12-08 17:40:27 +0300

    added comments and API entry points for making a room and joining a room

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+let app = express();
+
+app.use(express.json());
+
+
+//make a new match
+//and return the matchID, new player ID to the player, add matchID to the lobby
+//
+app.get("/nm",(req,res)=>{
+
+
+	//check if the game client is using the API
+	
+
+
+
+
+
+});
+
+
+//join a room
+app.post("/jr",(req,res)=>{
+
+	//check if the room is empty
+	
+
+
+
+
+})
